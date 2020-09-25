09638f938c6fc0f43e4cd4db1bfedbe21e6b9347
Sushil <sushilmangal731@gmail.com>
2020-09-25 16:16:29 +0530

    Updates

---
+++ precache-manifest.a5be1834973bc1040a10f008a6a778c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd150694781bb2f3b6b8",
+    "url": "/social-network/static/css/main.8a0c18df.chunk.css"
+  },
+  {
+    "revision": "cd150694781bb2f3b6b8",
+    "url": "/social-network/static/js/main.cd150694.chunk.js"
+  },
+  {
+    "revision": "e87098f1f9bf446b7ea4",
+    "url": "/social-network/static/css/1.c63eed32.chunk.css"
+  },
+  {
+    "revision": "e87098f1f9bf446b7ea4",
+    "url": "/social-network/static/js/1.e87098f1.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "525a2ae3f0a9f48ff87cd55bb846f73d",
+    "url": "/social-network/index.html"
+  }
+];
